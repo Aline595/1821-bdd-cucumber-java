e475080c56776c62d5809b7b9ec24852e8a6c394
Aline Soares da Silva <56769013+Aline595@users.noreply.github.com>
2021-07-14 11:32:44 -0300

    Update LeilaoTest.java

--- src/test/java/br/com/alura/leilao/model/LeilaoTest.java
+++ src/test/java/br/com/alura/leilao/model/LeilaoTest.java
@@ -10,17 +10,17 @@ import org.junit.jupiter.api.Test;
 public class LeilaoTest {
 	
 	
-	@DisplayName("um usuario pode dar lance em um leilao de outro usuario")
+	@DisplayName("um usuario pode dar lance em um leilao de outro usuario") // Descreve caso de teste
 	@Test
     public void deveReceberUmLance() {
-        Leilao leilao = new Leilao("Macbook Pro 15");
-        assertEquals(0, leilao.getLances().size());
+	Leilao leilao = new Leilao("Macbook Pro 15");
+	assertEquals(0, leilao.getLances().size());
 
-        BigDecimal doisMil = new BigDecimal("2000.0");
+	BigDecimal doisMil = new BigDecimal("2000.0");
 		leilao.propoe(new Lance(new Usuario("Steve Jobs"), doisMil));
 
-        assertEquals(1, leilao.getLances().size());
-        assertEquals(doisMil, leilao.getLances().get(0).getValor());
+	assertEquals(1, leilao.getLances().size());
+	assertEquals(doisMil, leilao.getLances().get(0).getValor());
     }
 	
 	
